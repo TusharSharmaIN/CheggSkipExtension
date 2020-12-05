20821d8a823eeb969ca9690e9b30505b22bcc787
Shubham Singh <36928095+shubhams167@users.noreply.github.com>
2020-12-06 03:46:27 +0530

    Added fetch of transcript

---
+++ expert_qna_script.js
@@ -0,0 +1,38 @@
+// Inject script to intercept console logs
+let script = document.createElement("script");
+script.src = chrome.runtime.getURL("inject.js");
+script.onload = function () {
+	this.remove();
+};
+(document.head || document.documentElement).appendChild(script);
+
+document.addEventListener("qidRead", e => {
+	const qid = e.detail;
+	if (!qid) return;
+	getTranscript(qid);
+});
+
+const initializeCountdown = () => {
+	const countdown = new Audio(chrome.runtime.getURL("countdown.mp3"));
+
+	// Get time left and log
+	const id = setInterval(function () {
+		const data = JSON.parse(window.localStorage.jStorage);
+		if (data.minutes == 0 && (data.seconds == "10" || data.seconds == "11")) {
+			countdown.play();
+			clearInterval(id);
+		}
+	}, 1000);
+};
+
+initializeCountdown();
+
+const getTranscript = qid => {
+	chrome.runtime.sendMessage({ command: "get-transcript", qid }, response => {
+		if (!response) {
+			console.log("Transcript not found");
+			return;
+		}
+		console.log(response);
+	});
+};
